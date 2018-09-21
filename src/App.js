c63f027aac535819c4104e680e052e62adb22027
hirdbluebird <bluebirdyourdestiny@gmail.com>
2018-09-21 05:26:45 +0300

    application is ready to go

--- src/App.js
+++ src/App.js
@@ -6,13 +6,64 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>RSVP</h1>
+          <p>A Treehouse App</p>
+          <form>
+              <input type="text" value="Safia" placeholder="Invite Someone" />
+              <button type="submit" name="submit" value="submit">Submit</button>
+          </form>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="main">
+          <div>
+            <h2>Invitees</h2>
+            <label>
+              <input type="checkbox" /> Hide those who havent responded
+            </label>
+          </div>
+          <table className="counter">
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>Unconfirmed:</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+          <ul>
+            <li className="pending"><span>Safia</span></li>
+            <li className="responded"><span>Iver</span>
+              <label>
+                <input type="checkbox" checked /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+            <li className="responded">
+              <span>Corrina</span>
+              <label>
+                <input type="checkbox" checked /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+            <li>
+              <span>Joel</span>
+              <label>
+                <input type="checkbox" /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
