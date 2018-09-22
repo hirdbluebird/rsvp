ab66046e0ce353cba5287d20ed97541216c873a1
hirdbluebird <bluebirdyourdestiny@gmail.com>
2018-09-22 22:53:04 +0300

    new users can be added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
 
   state = {
     isFiltered: false,
+    pendingGuest: '',
     guests: [
       {
         name: 'Treasure',
@@ -45,6 +46,9 @@ class App extends Component {
   toggleEditingAt = index =>
     this.toggleGuestPropertyAt('isEditing', index);
 
+  toggleFilter = () =>
+    this.setState({ isFiltered: !this.state.isFiltered});
+
   setNameAt = (name, indexToChange) =>
     this.setState({
       guests: this.state.guests.map((guest, index) => {
@@ -58,8 +62,23 @@ class App extends Component {
       })
     });
 
-  toggleFilter = () =>
-    this.setState({ isFiltered: !this.state.isFiltered});
+  handleNameInput = e =>
+    this.setState({ pendingGuest: e.target.value })
+
+  newGuestSubmitHandler = e => {
+    e.preventDefault();
+    this.setState({
+      guests: [
+        {
+          name: this.state.pendingGuest,
+          isConfirmed: false,
+          isEditing: false,
+        },
+        ...this.state.guests
+      ],
+      pendingGuest: ''
+    });
+  }
 
   getTotalInvited = () => this.state.guests.length;
   // getAttendingGuests = () =>
@@ -71,8 +90,12 @@ class App extends Component {
         <header>
           <h1>RSVP</h1>
           <p>A Treehouse App</p>
-          <form>
-              <input type="text" value="Safia" placeholder="Invite Someone" />
+          <form onSubmit={this.newGuestSubmitHandler}>
+              <input
+                type="text"
+                placeholder="Invite Someone"
+                value={this.state.pendingGuest}
+                onChange={this.handleNameInput} />
               <button type="submit" name="submit" value="submit">Submit</button>
           </form>
         </header>
